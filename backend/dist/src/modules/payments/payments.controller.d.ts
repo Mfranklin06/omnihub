5f1355a62c76c2d6e8e3c30f9ee17cd37b686eaf
Mfranklin06 <156433384+Mfranklin06@users.noreply.github.com>
2025-12-14 00:09:27 -0300

    Delete backend directory

--- backend/dist/src/modules/payments/payments.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { PaymentsService } from './payments.service';
-export declare class PaymentsController {
-    private readonly paymentsService;
-    constructor(paymentsService: PaymentsService);
-    handleMercadoPagoWebhook(id: string, topic: string, body: any): Promise<{
-        received: boolean;
-    }>;
-}
