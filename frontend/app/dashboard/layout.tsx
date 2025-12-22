ce2a67a5b642f5d58e34ea3b8997c7b35cde036c
Mfranklin06 <marconifilho2022@gmail.com>
2025-12-22 19:24:11 -0300

    feat: remove dynamic in layout

--- frontend/app/dashboard/layout.tsx
+++ frontend/app/dashboard/layout.tsx
@@ -1,5 +1,3 @@
-export const dynamic = 'force-dynamic'
-
 import Link from 'next/link'
 import { LayoutDashboard, ShoppingBag, LogOut, Package } from 'lucide-react'
 import { logout } from './actions'
