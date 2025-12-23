eee0e1d307f8f35cb4742faeb0cf9fb2fa4202b2
Mfranklin06 <marconifilho2022@gmail.com>
2025-12-22 21:12:12 -0300

    fix: change import products

--- frontend/app/components/AddToCardButton.tsx
+++ frontend/app/components/AddToCardButton.tsx
@@ -2,8 +2,8 @@
 
 import { Plus } from 'lucide-react'
 import { useCartStore } from '@/lib/store'
-import { toast } from 'sonner' // Opcional: Feedback visual
-import { Product } from '../dashboard/products/page'
+import { toast } from 'sonner'
+import { Product } from '../dashboard/products/ProductsOverview'
 
 export function AddToCartButton({ product }: { product: Product }) {
     const addToCart = useCartStore((state) => state.addToCart)
@@ -12,9 +12,7 @@ export function AddToCartButton({ product }: { product: Product }) {
         <button
             onClick={() => {
                 addToCart(product)
-                // Se quiser instalar: npm install sonner
                 toast.success('Adicionado ao carrinho')
-                //alert('Adicionado!') // Fallback simples
             }}
             className="bg-black text-white p-2 rounded-full hover:bg-gray-800 transition shadow-lg active:scale-90"
         >
